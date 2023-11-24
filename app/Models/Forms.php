82f812d90388f9d1658ebc936753b5656ff5a19a
Erick Alanes <90349878+ErickAlanesUwU@users.noreply.github.com>
2023-11-23 20:20:57 -0400

    Actualizacion de la ultima version del proyecto

--- app/Models/Forms.php
+++ app/Models/Forms.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+// Model para los formularios
 class Forms extends Model
 {
     use HasFactory;
